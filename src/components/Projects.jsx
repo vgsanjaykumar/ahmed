d0960eb1c5a438946b99b623626cf1ed2d60e8e9
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-18 00:57:26 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -48,7 +48,7 @@ export const Product = () => {
     };
     return (
         <section className="  top-container-1  ">
-            <div className="  ">
+            <div className="   ">
 
                 <div className="   ">
                     <h2 className="title-text font-bold  text-gray-600  mb-5">our top project</h2>
@@ -61,15 +61,15 @@ export const Product = () => {
 
                         <div
                             key={index}
-                            className="flex flex-col items-center md:space-x-4 space-x-0 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 group transform hover:-translate-y-2"
+                            className="flex flex-col items-center  rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 group transform hover:-translate-y-2"
                         >
 
-                            <div className=" px-6  w-50 h-25 md:h-50 lg:h-60  flex  items-center justify-center rounded-full mb-1 transition-all duration-300">
+                            <div className=" px-6  w-50 h-25 md:h-60 lg:h-80  flex object-fill items-center justify-center rounded-full mb-1 transition-all duration-300">
                                 <img src={d.icon} className=" h-40 w-80"/>
                             </div>
 
                             <div className="text-center ">
-                                <h3 className="fontmobile  p-0 md:p-5 md:text-lg font-bold text-gray-800 mb-2  hover:text-[#FFF200]">
+                                <h3 className="fontmobile   md:text-lg font-bold text-gray-800 mb-2  hover:text-[#FFF200]">
                                     {d.name}
                                 </h3>
                                 <p className="text-gray-600 md:text-sm fontmobile md:px-6 px-0 mb-1 md:mb-6 ">{d.text}</p>
